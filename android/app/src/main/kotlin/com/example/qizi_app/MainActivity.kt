d6479426c5e7c9c2148db8edc90e8880f1a6104e
Shuaib Toyeeb <toyyibatata90@gmail.com>
2024-07-14 07:31:12 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/example/qizi_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.qizi_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/data/questions.dart
@@ -0,0 +1,58 @@
+import 'package:qizi_app/model/quiz_question.dart';
+
+const question = [
+  QuizQuestion(
+    'How many times do Muslim prays in a day', [
+
+      'Five',
+      'Four',
+      'Six',
+      'Three',
+    ],
+  ),
+  QuizQuestion(
+    'What is Az-Zaqqum?', [
+
+      'Food for the people of hellfire',
+      'Home for the people of hellfire',
+      'Clothes for the people of hellfire',
+      'Drink for the people of hellfire',
+    ],
+  ),
+  QuizQuestion(
+    'What was the relation between Prophet Musa (alayhi as-salaam) & Prophet Haroon (alayhi as-salaam)?', [
+
+      'Brothers',
+      'Father & Sons',
+      'Friends',
+      'Cousins',
+    ],
+  ),
+  QuizQuestion(
+    'How many times do Muslim prays in a day', [
+
+      'Five',
+      'Four',
+      'Six',
+      'Three',
+    ],
+  ),
+  QuizQuestion(
+    'What is Az-Zaqqum?', [
+
+      'Food for the people of hellfire',
+      'Home for the people of hellfire',
+      'Clothes for the people of hellfire',
+      'Drink for the people of hellfire',
+    ],
+  ),
+  QuizQuestion(
+    'What was the relation between Prophet Musa (alayhi as-salaam) & Prophet Haroon (alayhi as-salaam)?', [
+
+      'Brothers',
+      'Father & Sons',
+      'Friends',
+      'Cousins',
+    ],
+  ),
+];
